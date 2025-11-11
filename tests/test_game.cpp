f22ec04addc5e0fec2d07df45e5dfe40980346a3
Louis989 <louisbedford22@gmail.com>
2025-11-11 13:14:40 +0000

    Add tests for winner detection in Game class

--- tests/test_game.cpp
+++ tests/test_game.cpp
@@ -26,3 +26,33 @@ TEST_CASE("checkWinner detects a simple row win") {
   CHECK(g.placeMove(2, 0, 'X'));
   CHECK(g.checkWinner() == 'X');
 }
+
+TEST_CASE("checkWinner detects a simple column win") {
+  Game g;
+  g.init(2);
+  CHECK(g.placeMove(1, 0, 'O'));
+  CHECK(g.placeMove(1, 1, 'O'));
+  CHECK(g.placeMove(1, 2, 'O'));
+  CHECK(g.checkWinner() == 'O');
+}
+
+TEST_CASE("checkWinner detects a diagonal win") {
+  Game g;
+  g.init(2);
+  CHECK(g.placeMove(0, 0, 'X'));
+  CHECK(g.placeMove(1, 1, 'X'));
+  CHECK(g.placeMove(2, 2, 'X'));
+  CHECK(g.checkWinner() == 'X');
+}
+
+TEST_CASE("checkWinner detects an antidiagonal win") {
+  Game g;
+  g.init(2);
+  CHECK(g.placeMove(0, 2, 'O'));
+  CHECK(g.placeMove(1, 1, 'O'));
+  CHECK(g.placeMove(2, 0, 'O'));
+  CHECK(g.checkWinner() == 'O');
+}
+
+
+
