925fc59f87ecf404d7311cc820d04556be0f8336
cgarryZA <christiangarry.southafrica@gmail.com>
2025-11-12 00:00:09 +0000

    Updated coverage script

--- scripts/coverage.bat
+++ scripts/coverage.bat
@@ -1,10 +1,5 @@
 @echo off
 cd /d "%~dp0.."
 
-rem run tests
-ctest --test-dir build
-
-rem at this point you have .gcda/.gcno in build\
-rem if you have gcov installed you can do, e.g.:
-rem gcov src\main.cpp
-rem gcov src\game.cpp
+rem run tests from the build dir, Debug config
+ctest --test-dir build -C Debug --output-on-failure
