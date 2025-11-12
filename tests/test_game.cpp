91b40f48f8effecfcdcbd09c4392a345c44e6dcb
Louis989 <louisbedford22@gmail.com>
2025-11-12 20:24:28 +0000

    Refactor board size tests and remove commented cases
    Refactor board size tests and cleanup commented code.

--- tests/test_game.cpp
+++ tests/test_game.cpp
@@ -1,24 +1,11 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "third_party/doctest.h"
-
 #include "game.h"
-// #include "board.h" // include later
-
-//TEST_CASE("boardSize grows with number of players") {
-  //CHECK(Game::boardSize(2) == 3);
-  //CHECK(Game::boardSize(3) == 4);
-  //CHECK(Game::boardSize(4) == 5);
-  //CHECK(Game::boardSize(5) == 6);
-  //CHECK(Game::boardSize(6) == 7);
-//}
 
 TEST_CASE("boardSize grows with number of players") {
+  Game g;
   for (int players = 2; players <=7; ++players) {
-    Game g;
-    g.init(players);
-    std::size_t size = g.checkWinner();
-    CHECK(g.boardSize(players) == g.board_.getSize());
-    CHECK(g.board_.getSize() == players + 1);
+    CHECK(g.boardSize(players) == players + 1);
   }
 }
 
@@ -32,56 +19,8 @@ TEST_CASE("placeMove respects bounds and occupancy on 3x3") {
 }
 
 
-/*
-TEST_CASE("checkWinner detects a simple row win") {
-  Game g;
-  g.init(2);
-  CHECK(g.placeMove(0, 0, 'X'));
-  CHECK(g.placeMove(1, 0, 'X'));
-  CHECK(g.placeMove(2, 0, 'X'));
-  CHECK(g.checkWinner() == 'X');
-}
-
-TEST_CASE("checkWinner detects a simple column win") {
-  Game g;
-  g.init(2);
-  CHECK(g.placeMove(1, 0, 'O'));
-  CHECK(g.placeMove(1, 1, 'O'));
-  CHECK(g.placeMove(1, 2, 'O'));
-  CHECK(g.checkWinner() == 'O');
-}
-
-TEST_CASE("checkWinner detects a diagonal win") {
-  Game g;
-  g.init(2);
-  CHECK(g.placeMove(0, 0, 'X'));
-  CHECK(g.placeMove(1, 1, 'X'));
-  CHECK(g.placeMove(2, 2, 'X'));
-  CHECK(g.checkWinner() == 'X');
-}
-
-TEST_CASE("checkWinner detects an antidiagonal win") {
-  Game g;
-  g.init(2);
-  CHECK(g.placeMove(0, 2, 'O'));
-  CHECK(g.placeMove(1, 1, 'O'));
-  CHECK(g.placeMove(2, 0, 'O'));
-  CHECK(g.checkWinner() == 'O');
-}
-
-TEST_CASE("checkWinner returns none when there is no win yet") {
-  Game g;
-  g.init(2);
-  g.placeMove(0, 0, 'X');
-  g.placeMove(0, 0, 'O');
-  g.placeMove(2, 2, 'X');
-  CHECK(g.checkWinner() == '\0');
-}
-
-*/
-
 TEST_CASE("detects wins on all board sizes") {
-  for (int players == 2; players <=7; ++players) {
+  for (int players = 2; players <=7; ++players) {
     Game g;
     g.init(players);
     int n = g.board_.getSize();
