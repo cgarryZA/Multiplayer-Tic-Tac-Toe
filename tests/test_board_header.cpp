57ecc8644274e42372c0672cd0c5ed52f7620644
cgarryZA <christiangarry.southafrica@gmail.com>
2025-11-13 10:44:58 +0000

    Added unit tests for header files

---
+++ tests/test_board_header.cpp
@@ -0,0 +1,4 @@
+// tests/test_board_header.cpp
+
+#include "board.h"
+#include "third_party/doctest.h"
---
+++ tests/test_game_header.cpp
@@ -0,0 +1,4 @@
+// tests/test_game_header.cpp
+
+#include "game.h"
+#include "third_party/doctest.h"
